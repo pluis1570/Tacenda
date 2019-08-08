806e22a97fc09d3cf6e256dd3d149aefe9d8db9e
Laurent Luypaert <laur.luyp@hotmail.com>
2019-08-08 15:22:35 +0200

    :)

--- ContractApp.Models/People/Consultant.cs
+++ ContractApp.Models/People/Consultant.cs
@@ -1,4 +1,5 @@
 ﻿using ContractApp.Models.Contracts;
+using ContractApp.Models.Skills;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +9,7 @@ namespace ContractApp.Models.People
     public class Consultant : Person
     {
         public PayrollContract Payroll { get; set; }
+        public Specialisation Title{ get; set; }
         public DateTime Birthdate { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime StopDate { get; set; }
---
+++ ContractApp.Models/Skills/Specialisation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContractApp.Models.Skills
+{
+    public class Specialisation
+    {
+        public string  Title { get; set; }
+    }
+}
